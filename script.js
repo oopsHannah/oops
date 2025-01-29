97b28161047c0bb8662db98aa4cce8b242c446d4
oopsHannah <75439693+oopsHannah@users.noreply.github.com>
2025-01-29 15:39:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function convertText() {
     const inputText = document.getElementById("inputText").value;
     const outputContainer = document.getElementById("outputContainer");
 
-    // Character mapping (Modify this to change replacements)
+    // Character mapping for Font 1 (Curly font)
     const charMap = {
         'a': 'Ꭿ', 'b': 'ᙖ', 'c': '᧙', 'd': 'ȡ', 'e': 'ꕊ',
         'f': 'ᖴ', 'g': '၅', 'h': 'ƕ',  'i': 'Ꭵ', 'j': '၂',
@@ -12,7 +12,27 @@ function convertText() {
         'H': 'Ƕ', 'I':'Ꮂ', 'Q': 'Ⴍ', 'T':'Ԏ', 'W':'ฬ', 
         'ee': 'ꔛ', 'EE':'ꔛ'
     };
-
+// Character mapping for Font 2 (Long font)
+    const charMap = {
+        'a': 'Ꭿ', 'b': 'ᙖ', 'c': '᧙', 'd': 'ȡ', 'e': 'ꕊ',
+        'f': 'ᖴ', 'g': '၅', 'h': 'ƕ',  'i': 'Ꭵ', 'j': '၂',
+        'k': '𐌺', 'l': 'ℓ', 'm': '᧗', 'n': 'ȵ', 'o': 'ᰔ',
+        'p': '𐬮', 'q': '৭', 'r': '𐊯', 's': 'ୡ', 't': 'ȶ',
+        'u': '৩', 'v': 'ⴸ', 'w': 'ຟ', 'x': '𑀌', 'y': 'ꌦ', 'z': '𐰁',
+        'H': 'Ƕ', 'I':'Ꮂ', 'Q': 'Ⴍ', 'T':'Ԏ', 'W':'ฬ', 
+        'ee': 'ꔛ', 'EE':'ꔛ'
+    };
+// Character mapping for Font 3 (Cutesy font)
+    const charMap = {
+        'a': 'Ꭿ', 'b': 'ᙖ', 'c': '᧙', 'd': 'ȡ', 'e': 'ꕊ',
+        'f': 'ᖴ', 'g': '၅', 'h': 'ƕ',  'i': 'Ꭵ', 'j': '၂',
+        'k': '𐌺', 'l': 'ℓ', 'm': '᧗', 'n': 'ȵ', 'o': 'ᰔ',
+        'p': '𐬮', 'q': '৭', 'r': '𐊯', 's': 'ୡ', 't': 'ȶ',
+        'u': '৩', 'v': 'ⴸ', 'w': 'ຟ', 'x': '𑀌', 'y': 'ꌦ', 'z': '𐰁',
+        'H': 'Ƕ', 'I':'Ꮂ', 'Q': 'Ⴍ', 'T':'Ԏ', 'W':'ฬ', 
+        'ee': 'ꔛ', 'EE':'ꔛ'
+    };
+    
     // Convert text using the mapping
     let convertedText = inputText.split('').map(char => {
         return charMap[char.toLowerCase()] || char;
